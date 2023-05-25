1489cb81cb6e955c1505f7e888f51bb1cb6aa49d
CommonSt <gcn450@naver.com>
2023-05-25 11:40:47 +0900

    한번해보는거

--- 실험용패키지/src/패키지인데이게뭔지모를패키지/MTITM.java
+++ 실험용패키지/src/패키지인데이게뭔지모를패키지/MTITM.java
@@ -3,7 +3,9 @@ package 패키지인데이게뭔지모를패키지;
 public class MTITM {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		for(int i=0; i<10; i++) {
+			System.out.print(20%3+i);
+		}
 
 	}
 
