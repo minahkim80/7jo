de9571d7401fbef67a15f9871d52603d87945cf1
minah80 <simplemina@naver.com>
2023-05-25 11:25:33 +0900

    잘해봅시다

--- 실험용패키지/src/패키지인데이게뭔지모를패키지/Mitne.java
+++ 실험용패키지/src/패키지인데이게뭔지모를패키지/Mitne.java
@@ -3,9 +3,8 @@ package 패키지인데이게뭔지모를패키지;
 public class Mitne {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
-		//잘 올리셨네요 앞으로 잘해 봅시다
+		System.out.println("우리가 젤 잘할꺼임");
+		System.out.println("프로젝트 잘합시다^^");
 
 	}
 
