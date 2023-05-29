f18a9adf2f5b77903ec546279e1ebe9eeb580f4f
CommonSt <134483596+CommonSt@users.noreply.github.com>
2023-05-29 22:44:15 +0900

    Update LottoManager.java
    조금만더 이제 출력되게하면 시작해야할듯해요

--- Lotto Project Package/src/lotto/LottoManager.java
+++ Lotto Project Package/src/lotto/LottoManager.java
@@ -95,7 +95,7 @@ public class LottoManager {
 
 		ArrayList<Integer> correctNumbers = new ArrayList<>();
 
-		// 당첨 번호와 구매 로또 번호를 비교하여 숫자 일치 여부 판단
+		// 당첨 번호와 구매 로또 번호를 비교하여 숫자 일치 여부 판단 +보너스 번호를 일단 다운로드 해볼
 		System.out.println("[내 로또 결과]");
 		for (int i = 0; i < numbers.size(); i++) {
 			if (winnerNumber.contains(numbers.get(i))) {
